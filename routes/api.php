a0e38dd04dd7508c83f9cf4a0ad4ee4e4724c240
Janilson Varela <janilsonjvs@gmail.com>
2023-01-07 18:46:10 -0300

    feat: rotas responsaveis

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AlunoController;
 use App\Http\Controllers\CidadeController;
 use App\Http\Controllers\EstadoController;
+use App\Http\Controllers\ResponsavelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,3 +26,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('/estados', EstadoController::class);
 Route::apiResource('/cidades', CidadeController::class);
 Route::apiResource('/alunos', AlunoController::class);
+Route::apiResource('/responsaveis', ResponsavelController::class);
